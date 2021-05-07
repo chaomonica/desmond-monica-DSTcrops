1ddb831098b41b3e5e2803dca37b57d493bb60b6
chaomonica <monica.y.chao@gmail.com>
2021-05-07 11:09:27 -0500

    initial commit

---
+++ src/main/java/com/galvanize/desmondmonicaDSTcrops/DesmondMonicaDsTcropsApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.desmondmonicaDSTcrops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesmondMonicaDsTcropsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesmondMonicaDsTcropsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/desmondmonicaDSTcrops/DesmondMonicaDsTcropsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.desmondmonicaDSTcrops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesmondMonicaDsTcropsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
